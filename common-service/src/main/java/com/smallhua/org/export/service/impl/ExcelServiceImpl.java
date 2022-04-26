6c8af3d16bb4d6164148e6c780dcfb552ab82f4c
zzh <964389099@qq.com>
2022-04-26 10:11:11 +0800

    优化创建目录

--- common-service/src/main/java/com/smallhua/org/export/service/impl/ExcelServiceImpl.java
+++ common-service/src/main/java/com/smallhua/org/export/service/impl/ExcelServiceImpl.java
@@ -1,5 +1,6 @@
 package com.smallhua.org.export.service.impl;
 
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.support.ExcelTypeEnum;
@@ -15,6 +16,9 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * 〈一句话功能简述〉<br>
@@ -45,10 +49,17 @@ public class ExcelServiceImpl implements ExcelService {
 
     @Override
     public void exportExcel() throws IOException {
-        File file = new File("E:/easyexcel", "testPageWrite.xlsx");
-        if (!file.exists()) file.createNewFile();
+        String filePath = "E:" + File.separator + "file" + DateUtil.format(DateUtil.date(), "yyyyMMdd");
+        String fileName = filePath + File.separator + "orderTest.xlsx";
+
+        System.out.println(fileName);
+
+        if(Files.notExists(Paths.get(filePath))) {
+            Files.createDirectories(Paths.get(filePath));
+        }
+        Files.createFile(Paths.get(fileName));
         ExcelWriter excelWriter = EasyExcel
-                .write(file, ExcelExportOrder.class)
+                .write(fileName, ExcelExportOrder.class)
                 .excelType(ExcelTypeEnum.XLSX).build();
 
         Long total = excelExportOrderExtMapper.queryTotalRecordsForExport();
@@ -56,4 +67,5 @@ public class ExcelServiceImpl implements ExcelService {
         EasyExcelUtils.pageWrite(excelWriter, "数据清单", total,
                 (currentPage, pageSize) -> excelExportOrderExtMapper.queryOrderForExport(currentPage, pageSize));
     }
+
 }
