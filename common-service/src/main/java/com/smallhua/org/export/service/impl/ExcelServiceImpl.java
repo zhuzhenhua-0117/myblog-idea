b0fc391d410e098029efb0c03a33e77b2e5fa55e
zzh <964389099@qq.com>
2022-04-26 10:21:17 +0800

    优化创建目录

--- common-service/src/main/java/com/smallhua/org/export/service/impl/ExcelServiceImpl.java
+++ common-service/src/main/java/com/smallhua/org/export/service/impl/ExcelServiceImpl.java
@@ -4,12 +4,12 @@ import cn.hutool.core.date.DateUtil;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.support.ExcelTypeEnum;
+import com.smallhua.org.domain.dto.ExcelExportOrder;
 import com.smallhua.org.domain.dto.ExportOrderForExcel;
 import com.smallhua.org.domain.strategy.CustomMergeStrategy;
 import com.smallhua.org.domain.mapper.ExcelExportOrderExtMapper;
 import com.smallhua.org.export.service.ExcelService;
 import com.smallhua.org.export.util.EasyExcelUtils;
-import com.smallhua.org.model.ExcelExportOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
