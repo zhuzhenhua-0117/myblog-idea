0d49479a1834e0c896fbb2e0d23c7e48e2e0ef9e
zzh <964389099@qq.com>
2022-10-09 17:30:15 +0800

    区间修改，单点查询

---
+++ zzh-8041-practice/src/main/java/com/smallhua/org/datastructure/tree/treeArray/IntervalSplit.java
@@ -0,0 +1,70 @@
+package com.smallhua.org.datastructure.tree.treeArray;
+
+/**
+ * 〈一句话功能简述〉<br> 区间修改，单点查询
+ *  区间统一加减某值 求操作之后某个索引下标的
+ * @author ZZH
+ * @create 2022/10/9
+ * @since 1.0.0
+ */
+public class IntervalSplit {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 5, 6, 9, 8, 3, 4};
+        PayContent payContent = new PayContent(nums);
+        System.out.println(payContent.query(2));
+
+        // 区间修改[2,4]的值 第2个到第四个增加4
+        payContent.modify(2, 4);
+        payContent.modify(5, -4);
+        System.out.println(payContent.query(2));
+        System.out.println(payContent.query(3));
+        System.out.println(payContent.query(4));
+        System.out.println(payContent.query(5));
+
+    }
+
+}
+
+class PayContent{
+
+    int[] h, a, b;
+    int len;
+
+    public PayContent(int[] nums){
+        a = nums;
+        this.len = nums.length;
+        b = new int[nums.length];
+        h = new int[nums.length + 1];
+        b[0] = a[0];
+        modify(1, b[0]);
+        for (int i = 1; i < nums.length; i++) {
+            b[i] = a[i] - a[i-1];
+            modify(i+1, b[i]);
+        }
+    }
+
+    private int lowBit(int index){return index & (-index);}
+
+    // index从1开始
+    public void modify(int index, int increment){
+        while(index <= len){
+            h[index] += increment;
+            index += lowBit(index);
+        }
+    }
+
+
+    // index从1开始
+    public int query(int index){
+        int ans = 0;
+        while(index > 0){
+            ans += h[index];
+            index -= lowBit(index);
+        }
+
+        return ans;
+    }
+
+
+}
