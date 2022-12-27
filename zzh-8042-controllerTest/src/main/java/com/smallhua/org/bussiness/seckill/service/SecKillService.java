1d8024ff059305c82a7bebd1dc47ba2210847e44
zhuzhenhua@benlai.com <964389099@qq.com>
2022-12-27 09:50:21 +0800

    去掉半成品

--- zzh-8042-controllerTest/src/main/java/com/smallhua/org/bussiness/seckill/service/SecKillService.java
+++
@@ -1,9 +0,0 @@
-package com.smallhua.org.bussiness.seckill.service;
-
-
-public interface SecKillService {
-
-    boolean putSku(long productId, long quality);
-
-    boolean executeSecKill(long productId);
-}
