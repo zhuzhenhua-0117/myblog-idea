1ccde63b7189f906e9979f13c0c182962c3b2264
zhuzhenhua@benlai.com <964389099@qq.com>
2022-12-29 18:14:13 +0800

    优化一版

--- zzh-8041-practice/src/main/java/com/smallhua/org/datastructure/tree/treeArray/IntervalUQ.java
+++ zzh-8041-practice/src/main/java/com/smallhua/org/datastructure/tree/treeArray/IntervalUQ.java
@@ -51,9 +51,8 @@ class PayContext{
         
         c = new int[len+1][2];
 
-        update(0, nums[0]);
-        for (int i = 1; i < nums.length; i++) {
-            update(i, nums[i] - nums[i - 1]);
+        for (int i = 0; i < nums.length; i++) {
+            update(i, (i > 0 ? nums[i] - nums[i - 1] : nums[i]));
         }
         
         
